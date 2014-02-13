9422bc3d113b7d3fecbbe2c875c2bb15bfd25c8b
Trevor Redfern <trevorredfern@gmail.com>
2014-02-12 21:32:43 -0800

    Deleted pork from project

--- terralien.py
+++ terralien.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
-import controllers
-
 import pork
 from pork.camera import Camera
 from pork.drawing import *
-from pork.scene import *
 
-pork.start()
-pork.engine().push_controller(pork.controllers.PerformanceController())
-pork.engine().push_controller(controllers.GameController())
-pork.run()
+import controllers
+
+def main():
+    pork.start()
+    pork.engine().push_controller(pork.controllers.PerformanceController())
+    pork.engine().push_controller(controllers.GameController())
+    pork.run()
+
+if __name__ == '__main__':
+    main()
