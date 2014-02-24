a3e394c6efaf1b19797b814ab116c0237165c883
Trevor Redfern <trevorredfern@gmail.com>
2014-02-23 20:47:27 -0800

    Cleaned everything out to the base line

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -1,29 +1,11 @@
-import pygsty.controllers
-from models.habitat import *
-from models.critter import *
+import pygsty
 
 class GameController(pygsty.controllers.BaseController):
-  def __init__(self):
-    self.map_info  = Map(10, 50000, 50000)
-    self.critters = []
+    def __init__(self):
+        pass
 
-    for r in range(300):
-      self.create_critter()
+    def draw(self):
+        pygsty.models.render_models()
 
-  def create_critter(self):
-    critter = Critter()
-    critter.speed = 5 + random.randrange(10)
-    critter.set_rotation(random.randrange(360))
-    h = random.randrange(len(self.map_info.habitats))
-    critter.occupy(self.map_info.habitats[h])
-    self.critters.append(critter)
-
-  def draw(self):
-    self.map_info.draw()
-    pygsty.models.render_models()
-
-  def update(self, dt):
-    for c in self.critters:
-      c.update()
-
-  
+    def update(self, dt):
+        pass
