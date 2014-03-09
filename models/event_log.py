bb4cdbbc169a56743018697cf0537bd5763432da
Trevor Redfern <trevorredfern@gmail.com>
2014-03-09 15:10:56 -0700

    Made it configurable to add events to the global file/stdout log

--- models/event_log.py
+++ models/event_log.py
@@ -1,5 +1,7 @@
+import pygsty
 
 _current_turn = 1
+_log_to_logger = True
 
 def reset_turn_counter():
     global _current_turn
@@ -14,6 +16,8 @@ class Entry():
     def __init__(self, message):
         self.message = message
         self.created_at = GameDate()
+        if _log_to_logger:
+            pygsty.logger.info(self.message)
 
 class GameDate():
     def __init__(self):
